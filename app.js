06d169ab48c0c182dc8e4a2d864f0a4f12e12fe1
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-10-06 05:50:12 +1300

    Implemented compareGuess and background colour changing

--- app.js
+++ app.js
@@ -4,28 +4,47 @@ container.classList.add("container");
 app.appendChild(container);
 
 const grid = [
-  [["s"], ["c"], ["a"], ["r"], ["p"]],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
+  ["s", "c", "a", "r", "f"],
+  ["s", "c", "r", "a", "p"],
+  ["~", "~", "~", "~", "~"],
+  ["~", "~", "~", "~", "~"],
+  ["~", "~", "~", "~", "~"],
+  ["~", "~", "~", "~", "~"],
 ];
 
 const word = "scrap";
 
-const guess = "scarp";
+const guess = "scarp".split("");
 
-const compareGuess = (guess, word) => {};
+const compareGuess = (guess, word) => {
+  let result = [];
+  for (let i = 0; i < guess.length; i++) {
+    let guessChar = guess[i];
+    let wordChar = word[i];
+    let charCorrectness = "X";
+    if (word.indexOf(guessChar) > -1) {
+      charCorrectness = "Y";
+      if (guessChar === wordChar) {
+        charCorrectness = "G";
+      }
+    }
+    result.push(charCorrectness);
+  }
+  return result;
+};
 
 const displayGrid = (grid) => {
   for (let row = 0; row < grid.length; row++) {
+    const backgrounds = compareGuess(grid[row], word);
     const rowElement = document.createElement("div");
     rowElement.classList.add("row");
     for (let col = 0; col < grid[row].length; col++) {
       const colElement = document.createElement("span");
       colElement.classList.add("letter-tile");
-      colElement.innerHTML = grid[row][col].toString().toUpperCase();
+      if (grid[row][col] !== "~") {
+        colElement.classList.add(backgrounds[col]);
+        colElement.innerHTML = grid[row][col].toUpperCase();
+      }
       rowElement.appendChild(colElement);
     }
     container.appendChild(rowElement);
@@ -33,3 +52,4 @@ const displayGrid = (grid) => {
 };
 
 displayGrid(grid);
+//console.log(compareGuess(["s", "c", "a", "r", "p"], ["s", "c", "r", "a", "f"]));
